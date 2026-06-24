454eea4c26b47243c50dc419b04081b7f9c13c00
ANTONY <amalcachilon@gmail.com>
2026-06-23 21:51:23 -0500

    V 1.1.

--- VENIDCOMED/Form1.Designer.cs
+++ VENIDCOMED/Form1.Designer.cs
@@ -44,148 +44,175 @@ namespace VENIDCOMED
         private void InitializeComponent()
         {
             components = new Container();
-            this.cmbCategoria = new ComboBox();
-            this.cmbProductos = new ComboBox();
-            this.dgvVentas = new DataGridView();
-            this.lblReloj = new Label();
-            this.timerReloj = new System.Windows.Forms.Timer(components);
-            this.txtCliente = new TextBox();
-            this.nudCantidad = new NumericUpDown();
-            this.rtbTicket = new RichTextBox();
-            this.lblTotalCaja = new Label();
-            this.txtPago = new TextBox();
-            this.lblVuelto = new Label();
-            this.btnNuevaOrden = new Button();
-            this.btnImprimirTicket = new Button();
-            this.btnRegistrar = new Button();
-            this.btnCerrarCaja = new Button();
-
-            ((ISupportInitialize)(this.dgvVentas)).BeginInit();
-            ((ISupportInitialize)(this.nudCantidad)).BeginInit();
-            this.SuspendLayout();
-
+            cmbCategoria = new ComboBox();
+            cmbProductos = new ComboBox();
+            dgvVentas = new DataGridView();
+            lblReloj = new Label();
+            timerReloj = new System.Windows.Forms.Timer(components);
+            txtCliente = new TextBox();
+            nudCantidad = new NumericUpDown();
+            rtbTicket = new RichTextBox();
+            lblTotalCaja = new Label();
+            txtPago = new TextBox();
+            lblVuelto = new Label();
+            btnNuevaOrden = new Button();
+            btnImprimirTicket = new Button();
+            btnRegistrar = new Button();
+            btnCerrarCaja = new Button();
+            ((ISupportInitialize)dgvVentas).BeginInit();
+            ((ISupportInitialize)nudCantidad).BeginInit();
+            SuspendLayout();
+            // 
             // cmbCategoria
-            this.cmbCategoria.Location = new Point(12, 12);
-            this.cmbCategoria.Name = "cmbCategoria";
-            this.cmbCategoria.Size = new Size(150, 23);
-            this.cmbCategoria.TabIndex = 0;
-            this.cmbCategoria.SelectedIndexChanged += new EventHandler(this.cmbCategoria_SelectedIndexChanged);
-
+            // 
+            cmbCategoria.Location = new Point(12, 12);
+            cmbCategoria.Name = "cmbCategoria";
+            cmbCategoria.Size = new Size(150, 23);
+            cmbCategoria.TabIndex = 0;
+            cmbCategoria.SelectedIndexChanged += cmbCategoria_SelectedIndexChanged;
+            // 
             // cmbProductos
-            this.cmbProductos.Location = new Point(168, 12);
-            this.cmbProductos.Name = "cmbProductos";
-            this.cmbProductos.Size = new Size(250, 23);
-            this.cmbProductos.TabIndex = 1;
-
-            // txtCliente
-            this.txtCliente.Location = new Point(12, 42);
-            this.txtCliente.Name = "txtCliente";
-            this.txtCliente.PlaceholderText = "Cliente";
-            this.txtCliente.Size = new Size(200, 23);
-
-            // nudCantidad
-            this.nudCantidad.Location = new Point(218, 42);
-            this.nudCantidad.Minimum = 1;
-            this.nudCantidad.Maximum = 1000;
-            this.nudCantidad.Value = 1;
-            this.nudCantidad.Name = "nudCantidad";
-            this.nudCantidad.Size = new Size(60, 23);
-
-            // btnRegistrar
-            this.btnRegistrar.Location = new Point(288, 42);
-            this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new Size(130, 23);
-            this.btnRegistrar.Text = "Registrar";
-            this.btnRegistrar.Click += new EventHandler(this.btnRegistrar_Click);
-
-            // rtbTicket
-            this.rtbTicket.Location = new Point(12, 80);
-            this.rtbTicket.Name = "rtbTicket";
-            this.rtbTicket.Size = new Size(406, 200);
-
+            // 
+            cmbProductos.Location = new Point(168, 12);
+            cmbProductos.Name = "cmbProductos";
+            cmbProductos.Size = new Size(250, 23);
+            cmbProductos.TabIndex = 1;
+            // 
             // dgvVentas
-            this.dgvVentas.Location = new Point(424, 12);
-            this.dgvVentas.Name = "dgvVentas";
-            this.dgvVentas.Size = new Size(480, 300);
-            this.dgvVentas.AllowUserToAddRows = false;
-
-            // lblTotalCaja
-            this.lblTotalCaja.Location = new Point(12, 290);
-            this.lblTotalCaja.Name = "lblTotalCaja";
-            this.lblTotalCaja.Size = new Size(300, 23);
-            this.lblTotalCaja.Text = "Caja Diaria: S/. 0.00";
-
-            // txtPago
-            this.txtPago.Location = new Point(12, 320);
-            this.txtPago.Name = "txtPago";
-            this.txtPago.PlaceholderText = "Pago";
-            this.txtPago.Size = new Size(100, 23);
-            this.txtPago.TextChanged += new EventHandler(this.txtPago_TextChanged);
-
-            // lblVuelto
-            this.lblVuelto.Location = new Point(118, 320);
-            this.lblVuelto.Name = "lblVuelto";
-            this.lblVuelto.Size = new Size(200, 23);
-            this.lblVuelto.Text = "Vuelto: S/. 0.00";
-
+            // 
+            dgvVentas.AllowUserToAddRows = false;
+            dgvVentas.Location = new Point(424, 12);
+            dgvVentas.Name = "dgvVentas";
+            dgvVentas.Size = new Size(480, 300);
+            dgvVentas.TabIndex = 6;
+            // 
             // lblReloj
-            this.lblReloj.Location = new Point(12, 350);
-            this.lblReloj.Name = "lblReloj";
-            this.lblReloj.Size = new Size(200, 23);
-            this.lblReloj.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-
+            // 
+            lblReloj.Location = new Point(12, 350);
+            lblReloj.Name = "lblReloj";
+            lblReloj.Size = new Size(200, 23);
+            lblReloj.TabIndex = 10;
+            lblReloj.Text = "23/06/2026 21:20:37";
+            // 
             // timerReloj
-            this.timerReloj.Interval = 1000;
-            this.timerReloj.Tick += new EventHandler(this.timerReloj_Tick);
-            this.timerReloj.Enabled = true;
-
+            // 
+            timerReloj.Enabled = true;
+            timerReloj.Interval = 1000;
+            timerReloj.Tick += timerReloj_Tick;
+            // 
+            // txtCliente
+            // 
+            txtCliente.Location = new Point(12, 42);
+            txtCliente.Name = "txtCliente";
+            txtCliente.PlaceholderText = "Cliente";
+            txtCliente.Size = new Size(200, 23);
+            txtCliente.TabIndex = 2;
+            // 
+            // nudCantidad
+            // 
+            nudCantidad.Location = new Point(218, 42);
+            nudCantidad.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            nudCantidad.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nudCantidad.Name = "nudCantidad";
+            nudCantidad.Size = new Size(60, 23);
+            nudCantidad.TabIndex = 3;
+            nudCantidad.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // rtbTicket
+            // 
+            rtbTicket.Location = new Point(12, 80);
+            rtbTicket.Name = "rtbTicket";
+            rtbTicket.Size = new Size(406, 200);
+            rtbTicket.TabIndex = 5;
+            rtbTicket.Text = "";
+            // 
+            // lblTotalCaja
+            // 
+            lblTotalCaja.Location = new Point(12, 290);
+            lblTotalCaja.Name = "lblTotalCaja";
+            lblTotalCaja.Size = new Size(300, 23);
+            lblTotalCaja.TabIndex = 7;
+            lblTotalCaja.Text = "Caja Diaria: S/. 0.00";
+            // 
+            // txtPago
+            // 
+            txtPago.Location = new Point(12, 320);
+            txtPago.Name = "txtPago";
+            txtPago.PlaceholderText = "Pago";
+            txtPago.Size = new Size(100, 23);
+            txtPago.TabIndex = 8;
+            txtPago.TextChanged += txtPago_TextChanged;
+            // 
+            // lblVuelto
+            // 
+            lblVuelto.Location = new Point(118, 320);
+            lblVuelto.Name = "lblVuelto";
+            lblVuelto.Size = new Size(200, 23);
+            lblVuelto.TabIndex = 9;
+            lblVuelto.Text = "Vuelto: S/. 0.00";
+            // 
             // btnNuevaOrden
-            this.btnNuevaOrden.Location = new Point(12, 380);
-            this.btnNuevaOrden.Name = "btnNuevaOrden";
-            this.btnNuevaOrden.Size = new Size(120, 23);
-            this.btnNuevaOrden.Text = "Nueva Orden";
-            this.btnNuevaOrden.Click += new EventHandler(this.btnNuevaOrden_Click);
-
+            // 
+            btnNuevaOrden.Location = new Point(12, 380);
+            btnNuevaOrden.Name = "btnNuevaOrden";
+            btnNuevaOrden.Size = new Size(120, 23);
+            btnNuevaOrden.TabIndex = 11;
+            btnNuevaOrden.Text = "Nueva Orden";
+            btnNuevaOrden.Click += btnNuevaOrden_Click;
+            // 
             // btnImprimirTicket
-            this.btnImprimirTicket.Location = new Point(138, 380);
-            this.btnImprimirTicket.Name = "btnImprimirTicket";
-            this.btnImprimirTicket.Size = new Size(140, 23);
-            this.btnImprimirTicket.Text = "Imprimir Ticket";
-            this.btnImprimirTicket.Click += new EventHandler(this.btnImprimirTicket_Click);
-
+            // 
+            btnImprimirTicket.Location = new Point(138, 380);
+            btnImprimirTicket.Name = "btnImprimirTicket";
+            btnImprimirTicket.Size = new Size(140, 23);
+            btnImprimirTicket.TabIndex = 12;
+            btnImprimirTicket.Text = "Imprimir Ticket";
+            btnImprimirTicket.Click += btnImprimirTicket_Click;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(288, 42);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(130, 23);
+            btnRegistrar.TabIndex = 4;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.Click += btnRegistrar_Click;
+            // 
             // btnCerrarCaja
-            this.btnCerrarCaja.Location = new Point(284, 380);
-            this.btnCerrarCaja.Name = "btnCerrarCaja";
-            this.btnCerrarCaja.Size = new Size(134, 23);
-            this.btnCerrarCaja.Text = "Cerrar Caja";
-            this.btnCerrarCaja.Click += new EventHandler(this.btnCerrarCaja_Click);
-
+            // 
+            btnCerrarCaja.Location = new Point(284, 380);
+            btnCerrarCaja.Name = "btnCerrarCaja";
+            btnCerrarCaja.Size = new Size(134, 23);
+            btnCerrarCaja.TabIndex = 13;
+            btnCerrarCaja.Text = "Cerrar Caja";
+            btnCerrarCaja.Click += btnCerrarCaja_Click;
+            // 
             // Form1
-            this.AutoScaleDimensions = new SizeF(7F, 15F);
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(920, 420);
-            this.Controls.Add(this.cmbCategoria);
-            this.Controls.Add(this.cmbProductos);
-            this.Controls.Add(this.txtCliente);
-            this.Controls.Add(this.nudCantidad);
-            this.Controls.Add(this.btnRegistrar);
-            this.Controls.Add(this.rtbTicket);
-            this.Controls.Add(this.dgvVentas);
-            this.Controls.Add(this.lblTotalCaja);
-            this.Controls.Add(this.txtPago);
-            this.Controls.Add(this.lblVuelto);
-            this.Controls.Add(this.lblReloj);
-            this.Controls.Add(this.btnNuevaOrden);
-            this.Controls.Add(this.btnImprimirTicket);
-            this.Controls.Add(this.btnCerrarCaja);
-            this.Name = "Form1";
-            this.Text = "VENID COMED";
-            this.Load += new EventHandler(this.Form1_Load);
-
-            ((ISupportInitialize)(this.dgvVentas)).EndInit();
-            ((ISupportInitialize)(this.nudCantidad)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(920, 420);
+            Controls.Add(cmbCategoria);
+            Controls.Add(cmbProductos);
+            Controls.Add(txtCliente);
+            Controls.Add(nudCantidad);
+            Controls.Add(btnRegistrar);
+            Controls.Add(rtbTicket);
+            Controls.Add(dgvVentas);
+            Controls.Add(lblTotalCaja);
+            Controls.Add(txtPago);
+            Controls.Add(lblVuelto);
+            Controls.Add(lblReloj);
+            Controls.Add(btnNuevaOrden);
+            Controls.Add(btnImprimirTicket);
+            Controls.Add(btnCerrarCaja);
+            Name = "Form1";
+            Text = "VENID COMED";
+            Load += Form1_Load;
+            ((ISupportInitialize)dgvVentas).EndInit();
+            ((ISupportInitialize)nudCantidad).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
